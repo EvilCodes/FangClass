559030069d085c6e4324cd4a60d01784921a0bc8
blackfox <huangy_tao@126.com>
2017-03-08 20:45:14 +0800

    创建商品详情界面对应的活动

---
+++ app/src/main/java/cn/ucai/blackfox/fangclass/View/activitys/ShopDetailsActivity.java
@@ -0,0 +1,15 @@
+package cn.ucai.blackfox.fangclass.View.activitys;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import cn.ucai.blackfox.fangclass.R;
+
+public class ShopDetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_shop_details);
+    }
+}
