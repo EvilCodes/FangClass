df188b71555e7e91cbdc009b7e5d670d8e6d3a02
blackfox <huangy_tao@126.com>
2017-03-13 22:07:38 +0800

    I接口的部分编写

--- app/src/main/java/cn/ucai/blackfox/fangclass/application/I.java
+++ app/src/main/java/cn/ucai/blackfox/fangclass/application/I.java
@@ -9,4 +9,17 @@ public interface I {
     final String rootUrl = "http://101.251.196.90:8080/FuLiCenterServerV2.0";
 
 
+    class Request {
+        final String FIND_NEW_AND_BOUTIQUE_GOODS = "/findNewAndBoutiqueGoods";
+        final String CAT_ID = "cat_id";
+        final String PAGE_ID = "page_id";
+        final String PAGE_SIZE = "page_size";
+
+
+
+
+    }
+
+
+
 }
