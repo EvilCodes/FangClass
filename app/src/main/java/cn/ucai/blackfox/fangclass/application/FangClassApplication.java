241bedb65e208b4590a41a0438f97f4a8a6858b0
blackfox <huangy_tao@126.com>
2017-03-13 22:05:44 +0800

    FangClassApplication的创建

---
+++ app/src/main/java/cn/ucai/blackfox/fangclass/application/FangClassApplication.java
@@ -0,0 +1,34 @@
+package cn.ucai.blackfox.fangclass.application;
+
+import android.app.Application;
+
+/**
+ * Created by Administrator on 2017/3/13 0013.
+ */
+
+public class FangClassApplication extends Application {
+    private static FangClassApplication instance;
+
+    private FangClassApplication() {
+
+    }
+
+    public static FangClassApplication getInstance() {
+        if (instance == null) {
+            synchronized (FangClassApplication.class) {
+                if (instance == null) {
+
+                    instance = new FangClassApplication();
+                }
+
+            }
+        }
+        return instance;
+
+    }
+
+
+
+
+
+}
