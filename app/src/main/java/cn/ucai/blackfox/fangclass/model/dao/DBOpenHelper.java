1d8fb207d1e46e8e6724c8d0ce7c2560f8243f27
blackfox <huangy_tao@126.com>
2017-03-14 15:56:10 +0800

    数据库三件套的创建

--- app/src/main/java/cn/ucai/blackfox/fangclass/model/dao/DBOpenHelper.java
+++ app/src/main/java/cn/ucai/blackfox/fangclass/model/dao/DBOpenHelper.java
@@ -1,8 +1,56 @@
 package cn.ucai.blackfox.fangclass.model.dao;
 
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
 /**
  * Created by Administrator on 2017/3/13 0013.
  */
 
-public class DBOpenHelper {
+public class DBOpenHelper extends SQLiteOpenHelper {
+    private static final int DATABASE_VERSION = 1;
+    private static final String FANGCLASS_USER_TABLE_CREATE = "CREATE TABLE" + UserDao.USER_TABLE_NAME + "("
+            + UserDao.USER_COLUMN_NAME + "TEXT PRIMARY KEY,"
+            + UserDao.USER_COLUMN_NICK + "TEXT,"
+            + UserDao.USER_COLUMN_AVATAR + "INTEGER,"
+            + UserDao.USER_COLUMN_AVATAR_PATH + "TEXT,"
+            + UserDao.USER_COLUMN_AVATAR_TYPE + "INTEGER,"
+            + UserDao.USER_COLUMN_AVATAR_SUFFIX + "TEXT,"
+            + UserDao.USER_COLUMN_AVATAR_UPDATE_TIME + "TEXT);";
+    private static String getUserDataBaseName() {
+        return "cn.ucai.blackfox.fangclass.db";
+    }
+
+    public DBOpenHelper(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, getUserDataBaseName(), factory, version);
+    }
+
+    private static DBOpenHelper instance;
+
+    public static DBOpenHelper getInstance(Context context) {
+
+        if (instance == null) {
+            synchronized (DBOpenHelper.class) {
+                if (instance == null) {
+                    instance = new DBOpenHelper(context, getUserDataBaseName(), null, DATABASE_VERSION);
+                }
+
+            }
+        }
+        return instance;
+    }
+
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+        sqLiteDatabase.execSQL(FANGCLASS_USER_TABLE_CREATE);
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+
+
+    }
 }
--- app/src/main/java/cn/ucai/blackfox/fangclass/model/dao/UserDao.java
+++ app/src/main/java/cn/ucai/blackfox/fangclass/model/dao/UserDao.java
@@ -1,8 +1,51 @@
 package cn.ucai.blackfox.fangclass.model.dao;
 
+import cn.ucai.blackfox.fangclass.application.FangClassApplication;
+import cn.ucai.blackfox.fangclass.model.domin.User;
+
 /**
  * Created by Administrator on 2017/3/13 0013.
  */
 
 public class UserDao {
+    /**
+     * 数据库操作用到的表名以及属性名
+     */
+    public static final String USER_TABLE_NAME = "t_fangclass_user";
+    public static final String USER_COLUMN_NAME = "m_user_name";
+    public static final String USER_COLUMN_NICK = "m_user_nick";
+    public static final String USER_COLUMN_AVATAR = "m_user_avatar_id";
+    public static final String USER_COLUMN_AVATAR_PATH = "m_user_avatar_path";
+    public static final String USER_COLUMN_AVATAR_SUFFIX = "m_user_avatar_suffix";
+    public static final String USER_COLUMN_AVATAR_TYPE = "m_user_avatar_type";
+    public static final String USER_COLUMN_AVATAR_UPDATE_TIME = "m_user_update_time";
+
+    private static UserDao instance;
+
+    public UserDao() {
+        DBManager.createDbOpenHelper(FangClassApplication.getInstance().getApplicationContext());
+
+    }
+    public static UserDao getInstance() {
+        if (instance == null) {
+            synchronized (UserDao.class) {
+                if (instance == null) {
+                    instance = new UserDao();
+                }
+            }
+        }
+        return instance;
+    }
+
+
+    private static boolean saveUser(User user) {
+        return DBManager.getInstance().saveUser(user);
+
+    }
+
+    public static User getUser(String username) {
+        return DBManager.getInstance().getUser(username);
+    }
+
+
 }
