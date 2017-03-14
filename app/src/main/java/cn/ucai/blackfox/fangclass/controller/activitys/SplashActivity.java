bf963f3a010392966a984ce43996fc8c5b094f69
blackfox <huangy_tao@126.com>
2017-03-14 17:48:44 +0800

    闪屏界面延时跳转到登录界面

--- app/src/main/java/cn/ucai/blackfox/fangclass/controller/activitys/SplashActivity.java
+++ app/src/main/java/cn/ucai/blackfox/fangclass/controller/activitys/SplashActivity.java
@@ -3,15 +3,38 @@ package cn.ucai.blackfox.fangclass.controller.activitys;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 import cn.ucai.blackfox.fangclass.R;
+import cn.ucai.blackfox.fangclass.model.utils.MFGT;
 
 public class SplashActivity extends AppCompatActivity {
+    Timer timer;
+    MyTask myTask;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        timer = new Timer();
+        myTask = new MyTask();
 
+    }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+        timer.schedule(myTask, 10000);
+    }
+
+    class MyTask extends TimerTask {
+
+        @Override
+        public void run() {
+            MFGT.jumptoLoginActivity(SplashActivity.this);
+//            添加跳转动画
+//            overridePendingTransition();
+        }
     }
 }
