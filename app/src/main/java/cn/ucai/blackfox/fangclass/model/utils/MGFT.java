ce269e4f9d25e05db1274381468851e9b7ffd1c4
blackfox <huangy_tao@126.com>
2017-03-14 16:45:54 +0800

    MGFT中添加跳转到登录界面的方法

---
+++ app/src/main/java/cn/ucai/blackfox/fangclass/model/utils/MGFT.java
@@ -0,0 +1,17 @@
+package cn.ucai.blackfox.fangclass.model.utils;
+
+import android.app.Activity;
+import android.content.Intent;
+
+import cn.ucai.blackfox.fangclass.controller.activitys.LoginActivity;
+
+/**
+ * Created by Administrator on 2017/3/14 0014.
+ */
+
+public class MGFT {
+    public static void jumptoLoginActivity(Activity activity) {
+        activity.startActivity(new Intent(activity, LoginActivity.class));
+    }
+
+}
