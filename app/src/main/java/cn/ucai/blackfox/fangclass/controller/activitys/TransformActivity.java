8bea95c7aff2be04ad222d7e95f1846cd72d989c
blackfox <huangy_tao@126.com>
2017-03-14 16:43:21 +0800

    创建首次安装应用闪屏后出现的TransformActivity

---
+++ app/src/main/java/cn/ucai/blackfox/fangclass/controller/activitys/TransformActivity.java
@@ -0,0 +1,15 @@
+package cn.ucai.blackfox.fangclass.controller.activitys;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import cn.ucai.blackfox.fangclass.R;
+
+public class TransformActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_translate);
+    }
+}
