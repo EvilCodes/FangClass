8fb4b6c37fc17e10484639ddd9b1c43b280e3572
blackfox <huangy_tao@126.com>
2017-03-15 11:08:24 +0800

    创建找回密码对应的活动窗口

---
+++ app/src/main/java/cn/ucai/blackfox/fangclass/controller/activitys/FindPassWordActivity.java
@@ -0,0 +1,28 @@
+package cn.ucai.blackfox.fangclass.controller.activitys;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
+
+import cn.ucai.blackfox.fangclass.R;
+import cn.ucai.blackfox.fangclass.model.utils.MFGT;
+
+/**
+ * Created by Administrator on 2017/3/15 0015.
+ */
+public class FindPassWordActivity extends AppCompatActivity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_find_password);
+        MFGT.goBackBeforeActivity(FindPassWordActivity.this);
+
+
+    }
+//    @Override
+//    protected void onStop() {
+//        super.onStop();
+//        finish();
+//    }
+}
