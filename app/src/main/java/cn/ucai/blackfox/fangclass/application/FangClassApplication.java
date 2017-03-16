54552c674d3c9202cf70f2cc738158cfc1d35d42
blackfox <huangy_tao@126.com>
2017-03-16 21:30:14 +0800

    FangClassApplication的测试修改

--- app/src/main/java/cn/ucai/blackfox/fangclass/application/FangClassApplication.java
+++ app/src/main/java/cn/ucai/blackfox/fangclass/application/FangClassApplication.java
@@ -8,21 +8,31 @@ import android.app.Application;
 
 public class FangClassApplication extends Application {
     private static FangClassApplication instance;
-    private FangClassApplication() {
 
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        instance =this;
     }
+
+    /**
+     * 有关全局变量的创建需要注意以下几个点：
+     * 1.onCreate()的覆写问题。
+     * 2.单例模式的特殊应用
+     * 3.在项目清单中进行相关的注册声明
+     *
+     * @return
+     */
+
     public static FangClassApplication getInstance() {
-        if (instance == null) {
-            synchronized (FangClassApplication.class) {
-                if (instance == null) {
-
-                    instance = new FangClassApplication();
-                }
-
-            }
-        }
+//        if (instance == null) {
+//            synchronized (FangClassApplication.class) {
+//                if (instance == null) {
+//                    instance = new FangClassApplication();
+//                }
+//            }
+//        }
         return instance;
-
     }
 
 
