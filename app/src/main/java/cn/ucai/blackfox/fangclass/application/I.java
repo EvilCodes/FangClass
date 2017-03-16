5786af2445b151326f41e5b56fcbbee16c9ad97d
blackfox <huangy_tao@126.com>
2017-03-16 21:25:57 +0800

    I接口中添加用于登录的相关参数

--- app/src/main/java/cn/ucai/blackfox/fangclass/application/I.java
+++ app/src/main/java/cn/ucai/blackfox/fangclass/application/I.java
@@ -9,12 +9,17 @@ public interface I {
     final String rootUrl = "http://101.251.196.90:8080/FuLiCenterServerV2.0";
 
 
-    class Request {
+    interface   Request {
+
         final String FIND_NEW_AND_BOUTIQUE_GOODS = "/findNewAndBoutiqueGoods";
         final String CAT_ID = "cat_id";
         final String PAGE_ID = "page_id";
         final String PAGE_SIZE = "page_size";
 
+
+        final String LOGIN = "/login";
+        final String USER_NAME = "m_user_name";
+        final String PASS_WORD = "m_user_password";
 
 
 
